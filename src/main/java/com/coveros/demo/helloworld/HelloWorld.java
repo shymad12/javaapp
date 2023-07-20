f78c3209987fb58ad5207b1ff80dae47af22c437
shymad12 <128215854+shymad12@users.noreply.github.com>
2023-07-20 14:47:07 +0530

    Update HelloWorld.java

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package com.coveros.demo.helloworld;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class HelloWorld {
+public class Hello World {
 
   public static void main(final String[] args) {
     final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
